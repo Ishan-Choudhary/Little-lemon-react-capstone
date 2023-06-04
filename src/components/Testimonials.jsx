d03c3bd34f2272b1abd54fab1a5f92a92b0789c1
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2023-06-04 16:55:16 +0530

    Testimonials section created and styled

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,46 @@
+import "../assests/styles/TestimonialStyle.css";
+
 import { useState, useEffect } from "react";
 
-const Results = ({ person }) => <h1>{person.name.first}</h1>;
+const Results = ({ person, message }) => {
+  const testimonialCardBg = {
+    width: "200px",
+    padding: "20px",
+    backgroundColor: "#fff",
+  };
+
+  const testimonialCardIntro = {
+    display: "flex",
+    margin: "20px 0px",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  };
+
+  return (
+    <div style={testimonialCardBg}>
+      <h4>Rating: ⭐️⭐️⭐️⭐️⭐️</h4>
+      <div style={testimonialCardIntro}>
+        <img
+          src={person.picture.thumbnail}
+          alt={`${person.name.first} photos`}
+          style={{ border: "solid #000 1px" }}
+        />
+        <h4 style={{ marginLeft: "10px" }}>{person.name.first}</h4>
+      </div>
+      <p>{message}</p>
+    </div>
+  );
+};
 
 const Testimonials = () => {
   const [usersData, setUsersData] = useState([]);
+
+  const testimonials = [
+    "Amazing food by the restaurant",
+    "Absolutely scrumptious dishes",
+    "The chefs did a fabulous job with the food",
+    "Will definitely eat here again",
+  ];
 
   useEffect(() => {
     const promises = [
@@ -16,18 +53,25 @@ const Testimonials = () => {
       fetch("https://randomuser.me/api/?inc=name,picture")
         .then((resp) => resp.json())
         .then((data) => data.results[0]),
+      fetch("https://randomuser.me/api/?inc=name,picture")
+        .then((resp) => resp.json())
+        .then((data) => data.results[0]),
     ];
 
-    Promise.all(promises).then((res) => setUsersData(res));
+    Promise.all(promises).then((res) => {
+      setUsersData(res);
+    });
   }, []);
 
   return (
-    <article>
+    <article className={"testimonial-bg"}>
       <h2>Testimonials</h2>
-      {usersData.map((curr, i) => (
-        <Results key={i} person={curr} />
-        // JUST FORMAT ABOVE DATA NOW EFFECT HOOK RUN TWICE
-      ))}
+      <div className="testimonial-cards">
+        {usersData.map((curr, i) => (
+          <Results key={i} person={curr} message={testimonials[i]} />
+          // JUST FORMAT ABOVE DATA NOW EFFECT HOOK RUN TWICE
+        ))}
+      </div>
     </article>
   );
 };
