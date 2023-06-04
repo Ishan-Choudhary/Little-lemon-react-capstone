9014712c3cffc53f390743b3bac2515d362a1d25
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2023-06-04 17:12:06 +0530

    Testimonials laptop small screen resolution issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/reservation" element={<Reservation />}></Route> /
+        <Route path="/reservation" element={<Reservation />}></Route>
       </Routes>
     </>
   );
