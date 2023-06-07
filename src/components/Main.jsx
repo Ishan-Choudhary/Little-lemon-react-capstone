2ba5b1cecd0cc5d387323349dd5b75a418df14da
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2023-06-07 18:59:13 +0530

    Footer issues fixed. Booking form component created and styled

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,13 @@ import Testimonials from "./Testimonials";
 import AboutMe from "./AboutMe";
 import Footer from "./Footer";
 
+import { useEffect } from "react";
+
 const Main = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <main>
       <Header />
--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,38 +1,21 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import BookingForm from "./BookingForm";
 
 const Reservation = () => {
   return (
-    <main style={{ height: "100vh" }}>
+    <main
+      style={{
+        minHeight: "100vh",
+        margin: "0",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#495e57",
+      }}
+    >
       <Header />
-      <article>
-        <form method="get">
-          <label for="date">Date</label>
-          <input type="date" required />
-          <label for="guestNum">Number of guests</label>
-          <input type="number" required />
-          <label for="seating">Seating</label>
-          <button id="seating" required>
-            Choose an option
-          </button>
-          <label for="occasion">Occasion</label>
-          <button id="occasion" required>
-            Choose an option
-          </button>
-        </form>
-        <div>
-          <h1>Available Times</h1>
-          <div
-            style={{
-              border: "solid 1px black",
-              width: "1px",
-              height: "1px",
-              padding: "150px",
-            }}
-          ></div>
-        </div>
-        <br />
-      </article>
+      <BookingForm />
       <Footer />
     </main>
   );
