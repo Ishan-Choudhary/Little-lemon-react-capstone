5f308b82c9cd329e309e19e3d1e1d5af45388262
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2023-06-07 23:04:25 +0530

    Form state created and set up to update

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,16 +1,25 @@
 import "../assests/styles/BookingFormStyle.css";
 import PrimaryBtn from "./Button/PrimaryBtn";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const BookingForm = () => {
-  const today = new Date().toISOString().split("T")[0];
+  const [currData, setCurrData] = useState({
+    date: "",
+    time: "",
+    guestNum: 0,
+    occasion: "",
+  });
 
-  console.log(today);
+  const today = new Date().toISOString().split("T")[0];
 
   useEffect(() => {
     window.scrollTo(0, 0);
   });
+
+  const updateValues = (e) => {
+    setCurrData({ ...currData, [e.target.id]: e.target.value });
+  };
 
   return (
     <article className={"form-bg"}>
@@ -18,17 +27,23 @@ const BookingForm = () => {
       <form method="get" className={"form-comp"}>
         <div>
           <label htmlFor="date">Date</label>
-          <input id="date" type="date" min={today} required />
+          <input
+            id="date"
+            type="date"
+            min={today}
+            required
+            onBlur={updateValues}
+          />
         </div>
         <div>
           <label htmlFor="time">Select a Time</label>
-          <select id="time">
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
+          <select id="time" onBlur={updateValues}>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
           </select>
         </div>
         <div>
@@ -40,16 +55,23 @@ const BookingForm = () => {
             min="1"
             max="10"
             required
+            onBlur={updateValues}
           />
         </div>
         <div>
           <label htmlFor="occasion">Choose an Occasion</label>
-          <select id="occasion">
-            <option>Birthday</option>
-            <option>Anniversary</option>
+          <select id="occasion" onBlur={updateValues}>
+            <option value="birthday">Birthday</option>
+            <option value="anniversary">Anniversary</option>
           </select>
         </div>
-        <PrimaryBtn content={"Submit Form"} />
+        <PrimaryBtn
+          content={"Submit Form"}
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(currData);
+          }}
+        />
       </form>
     </article>
   );
